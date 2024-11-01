4955dbb4a0985bcd2fbfadcb09aef219ca14106e
jtrocel <jtrocel@hansa.com.bo>
2024-11-01 10:35:30 -0400

    seccion 14 parte 1

---
+++ 007-admin-shop/src/api/tesloApi.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+
+const tesloApi = axios.create({
+    baseURL: import.meta.env.VITE_TESLO_API_URL,
+});
+
+/* console.log(import.meta.env); */
+
+export { tesloApi };
---
+++ 007-admin-shop/src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,17 @@
+<template>
+
+<TopMenu/>
+
+<RouterView></RouterView>
+
+<!-- Footer -->
+<FooterTemplate></FooterTemplate>
+
+
+</template>
+
+<script   lang="ts" setup>
+import TopMenu from '../components/TopMenu.vue';
+import FooterTemplate from '../components/FooterTemplate.vue';
+
+</script>
